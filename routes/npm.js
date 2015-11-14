d4da766aae8beb587a91b62777dc769f78cb8118
Kelly Selden <kellyselden@gmail.com>
2015-11-14 10:44:20 -0500

    add an npm dependencies endpoint

--- routes/npm.js
+++ routes/npm.js
@@ -3,24 +3,47 @@ var npm = require('npm');
 
 var router = express.Router();
 
-router.get('/:package', function(req, res) {
-  var package = req.params.package;
+function formatVersions(data) {
+  data = data[Object.keys(data)[0]].time;
+  delete data.modified;
+  delete data.created;
+  return data;
+}
 
+function formatDependencies(data) {
+  return data[Object.keys(data)[0]]['dependencies'];
+}
+
+router.get('/:package/:command', function(req, res) {
   npm.load(function(er, npm) {
     if (er) {
       console.log(er);
       return res.send(er);
     }
 
-    npm.commands.view([package, 'time'], true, function(er, data) {
+    var params = [req.params.package];
+    var transformFunc;
+
+    switch (req.params.command) {
+      case 'versions':
+        params.push('time');
+        transformFunc = formatVersions;
+        break;
+      case 'dependencies':
+        params.push('dependencies');
+        transformFunc = formatDependencies;
+        break;
+    }
+
+    npm.commands.view(params, true, function(er, data) {
       if (er) {
         console.log(er);
         return res.send(er);
       }
 
-      var time = data[Object.keys(data)[0]].time;
+      data = transformFunc(data);
 
-      res.send(time);
+      res.send(data);
     });
   });
 });
